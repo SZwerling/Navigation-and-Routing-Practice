138d724e628a29a3c08236f7ea4c90018637acc5
unknown <sazwerling@gmail.com>
2022-12-29 14:18:03 -0800

    implement prop-type library

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,34 @@
-// 'children' is a special name and prop. taken from what's placed between custom component tags.
+import PropTypes from 'prop-types'
 
 
-function Button({children}) {
+function Button({
+    children,
+    primary, 
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded
+}) {
     return(
         <button>{children}</button>
     )
 }
 
+Button.propTypes = {
+    checkForDuplicateColors: ({primary, secondary, success, warning, danger}) => {
+        const count = Number(!!primary) + Number(!!secondary) + Number(!!success) + Number(!!warning) + Number(!!danger) 
+        if(count > 1){
+            return new Error('invalid duplicate props')
+        }
+    }
+}
+
 
 export default Button;
+
+// 'children' is a special name and prop. taken from what's placed between custom component tags.
+
+// Number(true) equals 1.
+// !!undefined equals false. So Number(!!undefined) is 0.
