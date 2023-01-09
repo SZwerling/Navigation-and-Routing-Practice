b856f1ba03820f02ae7931890506bc0f409b46a8
unknown <sazwerling@gmail.com>
2023-01-08 20:33:26 -0800

    add id to each item in items

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+
 import Accordion from "./components/Accordion";
 
 function App() {
    const items = [
-      { label: "how is wet?", content: "like drippy" },
-      { label: "storms overhead", content: "like witches flew" },
-      { label: "off the grass", content: "and in the thickets wickets" },
+      { id: 1, label: "how is wet?", content: "like drippy" },
+      { id: 2, label: "storms overhead", content: "like witches flew" },
+      { id: 3, label: "off the grass", content: "and in the thickets wickets" },
    ];
    return <Accordion items={items}/>;
 }
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,9 +1,16 @@
-
+import React from "react";
 
 function Accordion({items}){
-    console.log({items})
+    const renderedItems = items.map((item) => {
+        return (
+            <React.Fragment key={item.id}>
+            <h3 className="italic text-lg">{item.label}</h3>
+            <div>{item.content}</div>
+            </React.Fragment>
+        )
+    })
     return(
-        <div>I am the accordion</div>
+        <div>{renderedItems}</div>
     )
 }
 
