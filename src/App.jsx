5ddff761bfa82cd6087023295ddb7a88e74d781d
unknown <sazwerling@gmail.com>
2023-01-09 22:59:32 -0800

    useState and conditional className in accordion

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Accordion from "./components/Accordion";
 function App() {
    const items = [
       { id: 1, label: "how is wet?", content: "like drippy" },
-      { id: 2, label: "storms overhead", content: "like witches flew" },
+      { id: 2, label: "storms overhead", content: "is how witches flew" },
       { id: 3, label: "off the grass", content: "and in the thickets wickets" },
    ];
    return <Accordion items={items}/>;
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Accordion({items}){
-    const renderedItems = items.map((item) => {
+    const [expandIndex, setExpandIndex] = useState(0)
+    const handleClick = (index) => setExpandIndex(index)
+    
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandIndex
         return (
-            <React.Fragment key={item.id}>
-            <h3 className="italic text-lg">{item.label}</h3>
-            <div>{item.content}</div>
+            <React.Fragment key={item.id} >
+            <h3 className="italic text-lg" onClick={() => handleClick(index)}>{item.label}</h3>
+          
+            <div className={`${isExpanded ? "italic text-lg" : ""}`}>{item.content}</div>
             </React.Fragment>
         )
     })
