9c7d0d62df6aa79aca917708f234a0ffe6a4c75b
unknown <sazwerling@gmail.com>
2023-01-11 15:24:19 -0800

    style accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,24 +1,31 @@
-import React, {useState} from "react";
-import { BiDownArrow, BiLeftArrow } from 'react-icons/bi';
+import React, { useState } from "react";
+import { BiDownArrow, BiLeftArrow } from "react-icons/bi";
 
-function Accordion({items}){
-    const [expandIndex, setExpandIndex] = useState(1)
-    
-    const renderedItems = items.map((item, index) => {
-        const isExpanded = index === expandIndex
-        const icon = <span>{isExpanded ? <BiDownArrow /> : <BiLeftArrow />}</span>
-    
-        return (
-            <React.Fragment key={item.id} >
-            <h3 className="italic text-lg cursor-pointer" onClick={() => setExpandIndex(index)}>{item.label}  {icon}</h3>
-            {isExpanded &&  <div>{item.content}</div>}
-            </React.Fragment>
-        )
-    })
-    return(
-        <div>{renderedItems}</div>
-    )
+function Accordion({ items }) {
+   const [expandIndex, setExpandIndex] = useState(1);
+
+   const renderedItems = items.map((item, index) => {
+      const isExpanded = index === expandIndex;
+      const icon = (
+         <span className="text-xl">
+            {isExpanded ? <BiDownArrow /> : <BiLeftArrow />}
+         </span>
+      );
+
+      return (
+         <React.Fragment key={item.id}>
+            <div
+               className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+               onClick={() => setExpandIndex(index)}
+            >
+               {item.label}
+               {icon}
+            </div>
+            {isExpanded && <div className="border-b p-5">{item.content}</div>}
+         </React.Fragment>
+      );
+   });
+   return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
-
 
 export default Accordion;
