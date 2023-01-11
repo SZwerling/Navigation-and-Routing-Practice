c54c20adc225f37a359209b554b3d1e493b0e803
unknown <sazwerling@gmail.com>
2023-01-11 15:17:05 -0800

    conditionally render arrow from react icons for accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,18 +1,17 @@
 import React, {useState} from "react";
+import { BiDownArrow, BiLeftArrow } from 'react-icons/bi';
 
 function Accordion({items}){
-    const [expandIndex, setExpandIndex] = useState(0)
-    const handleClick = (index) => setExpandIndex(index)
+    const [expandIndex, setExpandIndex] = useState(1)
     
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandIndex
+        const icon = <span>{isExpanded ? <BiDownArrow /> : <BiLeftArrow />}</span>
+    
         return (
             <React.Fragment key={item.id} >
-            <h3 className="italic text-lg" onClick={() => handleClick(index)}>{item.label}</h3>
-          {isExpanded &&
-           <div>{item.content}</div>
-          }
-           
+            <h3 className="italic text-lg cursor-pointer" onClick={() => setExpandIndex(index)}>{item.label}  {icon}</h3>
+            {isExpanded &&  <div>{item.content}</div>}
             </React.Fragment>
         )
     })
