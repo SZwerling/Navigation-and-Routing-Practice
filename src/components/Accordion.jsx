e99da9380813d0d731e6582a462e87ef94f4494d
unknown <sazwerling@gmail.com>
2023-01-12 15:15:40 -0800

    more complicated way to update expandIndex to avoid stale state value

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -3,13 +3,21 @@ import { BiDownArrow, BiLeftArrow } from "react-icons/bi";
 
 function Accordion({ items }) {
    const [expandIndex, setExpandIndex] = useState(-1);
+
+   //this more complicated way of setting state with a callback
+   //is only necessary if updating VERY quickly and need access
+   //to the most up-to-date state value.
+   //More simple and customary manner is below export statement.
    const handleClick = (index) => {
-    if(index === expandIndex){
-        setExpandIndex(-1)
-    } else {
-        setExpandIndex(index)
-    }
+      setExpandIndex((current) => {
+         if(current === index){
+            return -1
+         } else {
+            return index
+         }
+      })
    }
+
 
    const renderedItems = items.map((item, index) => {
       const isExpanded = index === expandIndex;
@@ -36,3 +44,11 @@ function Accordion({ items }) {
 }
 
 export default Accordion;
+
+// const handleClick = (index) => {
+//    if(index === expandIndex){
+//        setExpandIndex(-1)
+//    } else {
+//        setExpandIndex(index)
+//    }
+//   }
