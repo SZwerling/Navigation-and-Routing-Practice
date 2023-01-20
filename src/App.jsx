355f2d2bca1fbbe0faeefe8485a2e5686c6e52ad
unknown <sazwerling@gmail.com>
2023-01-19 17:08:17 -0800

    option state and handleOption in parent comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import ButtonPage from "./pages/ButtonPage";
 import AccordionPage from "./pages/AccordionPage";
 import Dropdown from "./components/Dropdown";
+import { useState } from "react";
+
 function App() {
+   const [selection, setSelection] = useState(null);
+   const handleSelect = (value) => {
+      setSelection(value);
+   };
+
    const options = [
       { id: 1, label: "Red", value: "red" },
       { id: 2, label: "Green", value: "green" },
@@ -12,7 +19,11 @@ function App() {
       <div>
          {/* <AccordionPage/>;
              <ButtonPage /> */}
-         <Dropdown options={options} />
+         <Dropdown
+            options={options}
+            selection={selection}
+            onSelect={handleSelect}
+         />
       </div>
    );
 }
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 
-function Dropdown({ options }) {
+function Dropdown({ options, selection, onSelect }) {
     const [isOpen, setIsOpen] = useState(false)
-    
+    const handleOptionClick = (value) => {
+        setIsOpen(false);
+        onSelect(value)
+    }
     const handleToggle = () => setIsOpen((currentIsOpen) => !currentIsOpen) // that very long discussion on updating 
                                                                             // a piece of state based on it's current state value
 
     const renderedOptions = options.map((option) => {
-        return <div key={option.id}>{option.label}</div>
+        return <div onClick={() => handleOptionClick(option)} key={option.id}>{option.label}</div>
     });
 
-    console.log(isOpen)
+    let content = selection ? selection.label : 'Select...'
     
     return (
         <div>
-            <div onClick={handleToggle}>select...</div>
+            <div onClick={handleToggle}>{content}</div>
             {isOpen && <div>{renderedOptions}</div>}
         </div>
     )
