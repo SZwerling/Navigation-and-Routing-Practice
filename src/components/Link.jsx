0815d72434a14f16409f76375967b74142987e6c
unknown <sazwerling@gmail.com>
2023-02-03 16:13:03 -0800

    handle ctrl and cmmd clicks on link

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -4,11 +4,15 @@ import NavigationContext from "../context/navigation";
 function Link({ to, children }){
     const { navigate } = useContext(NavigationContext)
     const handleClick = (event) => {
+        if(event.metaKey || event.ctrlKey){
+            return;
+        }
+
         event.preventDefault();
         navigate(to)
     }
     return (
-        <a onClick={handleClick} >{children}</a>
+        <a href={to} onClick={handleClick} >{children}</a>
     )
 }
 
