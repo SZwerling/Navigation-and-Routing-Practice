c35cf1907dc3566d2a604a0b31998a3f69894d84
unknown <sazwerling@gmail.com>
2023-02-07 13:28:04 -0800

    customize modal

--- src/pages/ModalPages.jsx
+++ src/pages/ModalPages.jsx
@@ -2,26 +2,35 @@ import Modal from "../components/Modal";
 import Button from "../components/Button";
 import { useState } from "react";
 
-function ModalPage(){
-    const [showModal, setShowModal] = useState(false)
+function ModalPage() {
+   const [showModal, setShowModal] = useState(false);
 
-    const handleClick = () => {
-        setShowModal(true)
-    }
+   const handleClick = () => {
+      setShowModal(true);
+   };
 
-    const onClose = () => {
-        setShowModal(false)
-    }
+   const onClose = () => {
+      setShowModal(false);
+   };
 
-    return(
-        <div>
-            <Button onClick={handleClick} primary>open modal</Button>
-            {showModal && <Modal onClose={onClose}/>}
-        </div>
-    )
+   const actionBar = <Button onClick={onClose} primary>I accept</Button>;
+
+   const modal = (
+      <Modal onClose={onClose} actionBar={actionBar}>
+         <p>Here is an important agreement for you to accept.</p>
+      </Modal>
+   );
+
+   return (
+      <div>
+         <Button onClick={handleClick} primary>
+            open modal
+         </Button>
+         {showModal && modal}
+      </div>
+   );
 }
 
 export default ModalPage;
-
 
 // This parent component will have the button and the state to show the button or not
