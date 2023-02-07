f8a1ef6f44ffabd0868492ebdbf70b41fdbeb093
unknown <sazwerling@gmail.com>
2023-02-07 13:31:44 -0800

    a little modal styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,26 @@
 import ReactDOM from "react-dom";
 
-function Modal({onClose, children, actionBar}) {
+function Modal({ onClose, children, actionBar }) {
    return ReactDOM.createPortal(
       <div>
-         <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
+         <div
+            onClick={onClose}
+            className="absolute inset-0 bg-gray-300 opacity-80"
+         ></div>
          <div className="absolute inset-40 p-10 bg-white">
-            {children}
-            {actionBar}
+            <div className="flex flex-col justify-between h-full">
+               {children}
+               <div className="flex justify-end">{actionBar}</div>
+            </div>
          </div>
       </div>,
-      document.querySelector('.modal-container')
+      document.querySelector(".modal-container")
    );
 }
 
 export default Modal;
 
-
 // create portal takes two arguments, the jsx, and where we want to put is in the index.html
 // we added a div at the end of the index.html with class of modal-container
-// We need to do this for the modal becuase if any parent container has position other than static, it will be 
+// We need to do this for the modal becuase if any parent container has position other than static, it will be
 // contained within that parent container and not take up the entire screen
