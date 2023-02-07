fa9304d82daefc139f7a16fbfa63f76c7ca3a41c
unknown <sazwerling@gmail.com>
2023-02-07 15:55:43 -0800

    table styled but not reusable

---
+++ src/pages/TablePage.jsx
@@ -0,0 +1,16 @@
+import Table from "../components/Table";
+
+function TablePage(){
+    const data = [
+        {name: "orange", color: 'bg-orange-500', score: 5},
+        {name: "apple", color: 'bg-red-500', score: 3},
+        {name: "banana", color: 'bg-yellow-500', score: 1},
+        {name: "lime", color: 'bg-green-500', score: 4}
+    ]
+
+    return(
+        <Table data={data}/>
+    )
+}
+
+export default TablePage;
