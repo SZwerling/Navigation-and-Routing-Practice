c26f971772b730844e4f0a97ef7ee75aa5f59ded
unknown <sazwerling@gmail.com>
2023-02-22 18:54:21 -0800

    basic styling for counter form

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,18 +1,35 @@
-import useCounter from "../hooks/use-counter";
+//import useCounter from "../hooks/use-counter"; taking out custom hook so we can learn useReducer
 import Button from "../components/Button";
-
+import { useState } from "react";
+import Panel from "../components/Panel";
 
 
 
 function CounterPage({ initialCount }){
 
-    const {count, increment} = useCounter(initialCount)
+    const [count, setCount] = useState(initialCount)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+    const decrement = () => {
+        setCount(count - 1)
+    }
     
     return(
-        <div>
-            <h1>count is {count}</h1>
+        <Panel className="m-3">
+            <h1 className="text-lg">count is {count}</h1>
+            <div className="flex flex-row">
             <Button primary rounded className={'mb-5 hover:bg-sky-700'} onClick={increment}>increment</Button>
-        </div>
+            <Button primary rounded className={'mb-5 hover:bg-sky-700'} onClick={decrement}>decrement</Button>
+            </div>
+            <form action="">
+                <label>Custom Add!</label>
+                    <input type="number" className="p-1 m-3 bg-gray-50 border border-grey-300"/>
+                
+                <Button primary rounded>Add It!!</Button>
+            </form>
+        </Panel>
     )
 }
 
