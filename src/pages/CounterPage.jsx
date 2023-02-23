3f870d6dbf94a803f70c340dbe258028d76111ea
unknown <sazwerling@gmail.com>
2023-02-22 19:09:38 -0800

    counter page using just useState

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -3,34 +3,66 @@ import Button from "../components/Button";
 import { useState } from "react";
 import Panel from "../components/Panel";
 
+function CounterPage({ initialCount }) {
+   const [count, setCount] = useState(initialCount);
+   const [valueToAdd, setValueToAdd] = useState(1);
+
+   const increment = () => {
+      setCount(count + 1);
+   };
+   const decrement = () => {
+      setCount(count - 1);
+   };
 
 
-function CounterPage({ initialCount }){
 
-    const [count, setCount] = useState(initialCount)
+   const handleChange = (e) => {
+    const value = parseInt(e.target.value) || 0;
+    setValueToAdd(value)
+   }
 
-    const increment = () => {
-        setCount(count + 1)
-    }
-    const decrement = () => {
-        setCount(count - 1)
-    }
-    
-    return(
-        <Panel className="m-3">
-            <h1 className="text-lg">count is {count}</h1>
-            <div className="flex flex-row">
-            <Button primary rounded className={'mb-5 hover:bg-sky-700'} onClick={increment}>increment</Button>
-            <Button primary rounded className={'mb-5 hover:bg-sky-700'} onClick={decrement}>decrement</Button>
-            </div>
-            <form action="">
-                <label>Custom Add!</label>
-                    <input type="number" className="p-1 m-3 bg-gray-50 border border-grey-300"/>
-                
-                <Button primary rounded>Add It!!</Button>
-            </form>
-        </Panel>
-    )
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    setCount(count + valueToAdd);
+    setValueToAdd(0)
+   }
+
+   return (
+      <Panel className="m-3">
+         <h1 className="text-lg">count is {count}</h1>
+         <div className="flex flex-row">
+            <Button
+               primary
+               rounded
+               className={"mb-5 hover:bg-sky-700"}
+               onClick={increment}
+            >
+               increment
+            </Button>
+            <Button
+               primary
+               rounded
+               className={"mb-5 hover:bg-sky-700"}
+               onClick={decrement}
+            >
+               decrement
+            </Button>
+         </div>
+         <form onSubmit={handleSubmit}>
+            <label>Custom Add!</label>
+            <input
+               onChange={handleChange}
+               value={valueToAdd || ""}
+               type="number"
+               className="p-1 m-3 bg-gray-50 border border-grey-300"
+            />
+
+            <Button primary rounded>
+               Add It!!
+            </Button>
+         </form>
+      </Panel>
+   );
 }
 
 export default CounterPage;
