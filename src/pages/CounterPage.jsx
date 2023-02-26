81f490e2face523057232af21ddd457c482c7048
unknown <sazwerling@gmail.com>
2023-02-25 16:11:40 -0800

    start refactor from useState to useReducer

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,35 +1,43 @@
 //import useCounter from "../hooks/use-counter"; taking out custom hook so we can learn useReducer
 import Button from "../components/Button";
-import { useState } from "react";
+import { useReducer } from "react";
 import Panel from "../components/Panel";
 
+const reducer = (state, action) => {
+
+}
+
 function CounterPage({ initialCount }) {
-   const [count, setCount] = useState(initialCount);
-   const [valueToAdd, setValueToAdd] = useState(1);
+   // const [count, setCount] = useState(initialCount);
+   // const [valueToAdd, setValueToAdd] = useState(1);
+   const [state, dispatch] = useReducer(reducer, {
+      count: initialCount,
+      valueToAdd: 0
+   })
 
    const increment = () => {
-      setCount(count + 1);
+      // setCount(count + 1);
    };
    const decrement = () => {
-      setCount(count - 1);
+      // setCount(count - 1);
    };
 
 
 
    const handleChange = (e) => {
     const value = parseInt(e.target.value) || 0;
-    setValueToAdd(value)
+   //  setValueToAdd(value)
    }
 
    const handleSubmit = (e) => {
     e.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0)
+   //  setCount(count + valueToAdd);
+   //  setValueToAdd(0)
    }
 
    return (
       <Panel className="m-3">
-         <h1 className="text-lg">count is {count}</h1>
+         <h1 className="text-lg">count is {state.count}</h1>
          <div className="flex flex-row">
             <Button
                primary
@@ -52,7 +60,7 @@ function CounterPage({ initialCount }) {
             <label>Custom Add!</label>
             <input
                onChange={handleChange}
-               value={valueToAdd || ""}
+               value={state.valueToAdd || ""}
                type="number"
                className="p-1 m-3 bg-gray-50 border border-grey-300"
             />
